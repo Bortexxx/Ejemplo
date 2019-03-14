23570e9c32da8a800579ceb3c62b419a7a798ed4
Jonathan Alberto Perez Nava <thejohn@jonathans-imac.local>
2019-03-14 11:37:07 -0700

    Hola mundo Comentario

--- ibmCalc/ibmCalc/ViewController.swift
+++ ibmCalc/ibmCalc/ViewController.swift
@@ -19,6 +19,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //Hola Mundo
     }
     @IBAction func Go(_ sender: Any) {
         let h = Double(alturaTxt.text!)
